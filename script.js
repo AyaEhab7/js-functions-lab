46d9b17c25604a0685e87b3e07f5e54a506b53cd
aya ehab <ayaaehab1999@gmail.com>
2024-09-24 17:04:08 +0300

    this is my commit message

---
+++ script.js
@@ -0,0 +1,34 @@
+//Exercise 1:  
+const maxOfTwoNumbers = (x, y) => {
+    if (x >= y) {
+      return x;
+    } else {
+      return y;
+    }
+  }
+//Exercise 2:  
+function isAdult (age){ 
+    return age >= 18 ? 'Adult' : 'Minor';
+}
+
+//Exercise 3:
+// 'aeiou
+
+function isCharAVowel(char):
+ if(char = 'a' ||'e'||'i'||'o'||'u'){
+    return 
+ }else{
+    return false
+ }
+
+ //Exercise 4:
+
+
+
+
+  console.log('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+  console.log('Exercise 2 Result:', isAdult(21));
+  console.log('Exercise 3 Result:', isCharAVowel("a"));
+  console.log('Exercise 4 Result:', generateEmail("johnsmith", "example.com"));
+
+
